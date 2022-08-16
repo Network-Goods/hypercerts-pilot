13870b480e3f0854bed005fa5efccb28fc0f0e63
Jip Stavenuiter <jipstavenuiter@gmail.com>
2022-08-16 20:52:07 +0200

    Repo setup (#4)
    * Initial commit from Create Next App
    
    * add prettier and quiver
    
    * add chakra-ui
    
    * add formik
    
    * add quiver test component

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Text } from "@chakra-ui/react";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Text fontStyle="h1">Welcome to hypercerts</Text>
+    </div>
+  );
+};
+
+export default Home;
