e4d8f4e8ffc4914cdeea6ef1349fd6e68e874932
Matthew O'Connell <totallyterrific@tutanota.com>
2022-08-22 13:57:28 +0200

    feat(graph): initial commit

---
+++ graph/src/hypercert-minter-v-0.ts
@@ -0,0 +1,143 @@
+import {
+  AdminChanged,
+  ApprovalForAll,
+  BeaconUpgraded,
+  ImpactClaimed,
+  Initialized,
+  RoleAdminChanged,
+  RoleGranted,
+  RoleRevoked,
+  TransferBatch,
+  TransferSingle,
+  URI,
+  Upgraded,
+  RightAdded,
+  WorkScopeAdded,
+  ImpactScopeAdded,
+} from "../generated/HypercertMinterV0/HypercertMinterV0";
+import { Hypercert, HypercertBalance, ImpactScope, Right, WorkScope } from "../generated/schema";
+
+export function handleAdminChanged(event: AdminChanged): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  // let entity = ExampleEntity.load(event.transaction.from.toHex())
+  // // Entities only exist after they have been saved to the store;
+  // // `null` checks allow to create entities on demand
+  // if (!entity) {
+  //   entity = new ExampleEntity(event.transaction.from.toHex())
+  //   // Entity fields can be set using simple assignments
+  //   entity.count = BigInt.fromI32(0)
+  // }
+  // // BigInt and BigDecimal math are supported
+  // entity.count = entity.count + BigInt.fromI32(1)
+  // // Entity fields can be set based on event parameters
+  // entity.previousAdmin = event.params.previousAdmin
+  // entity.newAdmin = event.params.newAdmin
+  // // Entities can be written to the store with `.save()`
+  // entity.save()
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.DEFAULT_ADMIN_ROLE(...)
+  // - contract.UPGRADER_ROLE(...)
+  // - contract.balanceOf(...)
+  // - contract.balanceOfBatch(...)
+  // - contract.contributorImpacts(...)
+  // - contract.counter(...)
+  // - contract.exists(...)
+  // - contract.getImpactCert(...)
+  // - contract.getRoleAdmin(...)
+  // - contract.hasRole(...)
+  // - contract.impactScopes(...)
+  // - contract.isApprovedForAll(...)
+  // - contract.proxiableUUID(...)
+  // - contract.rights(...)
+  // - contract.supportsInterface(...)
+  // - contract.totalSupply(...)
+  // - contract.uri(...)
+  // - contract.version(...)
+  // - contract.workScopes(...)
+}
+
+export function handleApprovalForAll(event: ApprovalForAll): void {}
+
+export function handleBeaconUpgraded(event: BeaconUpgraded): void {}
+
+export function handleImpactClaimed(event: ImpactClaimed): void {
+  let entity = new Hypercert(event.params.id.toString());
+  entity.claimHash = event.params.claimHash;
+  entity.contributors = changetype<Array<string>>(event.params.contributors);
+  entity.impactDateFrom = event.params.impactTimeframe[0];
+  entity.impactDateTo = event.params.impactTimeframe[1];
+  entity.impactScopes = event.params.impactScopes;
+  entity.rights = event.params.rights;
+  entity.uri = event.params.uri;
+  entity.version = event.params.version;
+  entity.workDateFrom = event.params.workTimeframe[0];
+  entity.workDateTo = event.params.workTimeframe[1];
+  entity.workScopes = event.params.workScopes;
+  entity.save();
+}
+
+export function handleImpactScopeAdded(event: ImpactScopeAdded): void {
+  let entity = new ImpactScope(event.params.id);
+  entity.text = event.params.text.toString();
+  entity.save();
+}
+
+export function handleInitialized(event: Initialized): void {}
+
+export function handleRoleAdminChanged(event: RoleAdminChanged): void {}
+
+export function handleRoleGranted(event: RoleGranted): void {}
+
+export function handleRoleRevoked(event: RoleRevoked): void {}
+
+export function handleRightAdded(event: RightAdded): void {
+  let entity = new Right(event.params.id);
+  entity.text = event.params.text.toString();
+  entity.save();
+}
+
+export function handleWorkScopeAdded(event: WorkScopeAdded): void {
+  let entity = new WorkScope(event.params.id);
+  entity.text = event.params.text.toString();
+  entity.save();
+}
+
+export function handleTransferBatch(event: TransferBatch): void {}
+
+export function handleTransferSingle(event: TransferSingle): void {
+  let balanceFrom = HypercertBalance.load(event.params.from);
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (balanceFrom) {
+    // Entity fields can be set using simple assignments
+    balanceFrom.amount = balanceFrom.amount - event.params.value;
+    balanceFrom.save();
+  }
+
+  let balanceTo = HypercertBalance.load(event.params.to);
+  if (!balanceTo) {
+    // Entity fields can be set using simple assignments
+    balanceTo = new HypercertBalance(event.params.to);
+  }
+
+  balanceTo.amount = balanceTo.amount + event.params.value;
+  balanceTo.save();
+}
+
+export function handleURI(event: URI): void {}
+
+export function handleUpgraded(event: Upgraded): void {}
---
+++ graph/tests/hypercert-minter-v-0-utils.ts
@@ -0,0 +1,334 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt, Bytes } from "@graphprotocol/graph-ts"
+import {
+  AdminChanged,
+  ApprovalForAll,
+  BeaconUpgraded,
+  ImpactBurned,
+  ImpactClaimed,
+  Initialized,
+  RoleAdminChanged,
+  RoleGranted,
+  RoleRevoked,
+  TransferBatch,
+  TransferSingle,
+  URI,
+  Upgraded
+} from "../generated/HypercertMinterV0/HypercertMinterV0"
+
+export function createAdminChangedEvent(
+  previousAdmin: Address,
+  newAdmin: Address
+): AdminChanged {
+  let adminChangedEvent = changetype<AdminChanged>(newMockEvent())
+
+  adminChangedEvent.parameters = new Array()
+
+  adminChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousAdmin",
+      ethereum.Value.fromAddress(previousAdmin)
+    )
+  )
+  adminChangedEvent.parameters.push(
+    new ethereum.EventParam("newAdmin", ethereum.Value.fromAddress(newAdmin))
+  )
+
+  return adminChangedEvent
+}
+
+export function createApprovalForAllEvent(
+  account: Address,
+  operator: Address,
+  approved: boolean
+): ApprovalForAll {
+  let approvalForAllEvent = changetype<ApprovalForAll>(newMockEvent())
+
+  approvalForAllEvent.parameters = new Array()
+
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromBoolean(approved))
+  )
+
+  return approvalForAllEvent
+}
+
+export function createBeaconUpgradedEvent(beacon: Address): BeaconUpgraded {
+  let beaconUpgradedEvent = changetype<BeaconUpgraded>(newMockEvent())
+
+  beaconUpgradedEvent.parameters = new Array()
+
+  beaconUpgradedEvent.parameters.push(
+    new ethereum.EventParam("beacon", ethereum.Value.fromAddress(beacon))
+  )
+
+  return beaconUpgradedEvent
+}
+
+export function createImpactBurnedEvent(impactCertID: BigInt): ImpactBurned {
+  let impactBurnedEvent = changetype<ImpactBurned>(newMockEvent())
+
+  impactBurnedEvent.parameters = new Array()
+
+  impactBurnedEvent.parameters.push(
+    new ethereum.EventParam(
+      "impactCertID",
+      ethereum.Value.fromUnsignedBigInt(impactCertID)
+    )
+  )
+
+  return impactBurnedEvent
+}
+
+export function createImpactClaimedEvent(
+  id: BigInt,
+  claimHash: Bytes,
+  contributors: Array<Address>,
+  workTimeframe: Array<BigInt>,
+  impactTimeframe: Array<BigInt>,
+  workScopes: Array<BigInt>,
+  impactScopes: Array<BigInt>,
+  version: BigInt,
+  uri: string
+): ImpactClaimed {
+  let impactClaimedEvent = changetype<ImpactClaimed>(newMockEvent())
+
+  impactClaimedEvent.parameters = new Array()
+
+  impactClaimedEvent.parameters.push(
+    new ethereum.EventParam("id", ethereum.Value.fromUnsignedBigInt(id))
+  )
+  impactClaimedEvent.parameters.push(
+    new ethereum.EventParam(
+      "claimHash",
+      ethereum.Value.fromFixedBytes(claimHash)
+    )
+  )
+  impactClaimedEvent.parameters.push(
+    new ethereum.EventParam(
+      "contributors",
+      ethereum.Value.fromAddressArray(contributors)
+    )
+  )
+  impactClaimedEvent.parameters.push(
+    new ethereum.EventParam(
+      "workTimeframe",
+      ethereum.Value.fromUnsignedBigIntArray(workTimeframe)
+    )
+  )
+  impactClaimedEvent.parameters.push(
+    new ethereum.EventParam(
+      "impactTimeframe",
+      ethereum.Value.fromUnsignedBigIntArray(impactTimeframe)
+    )
+  )
+  impactClaimedEvent.parameters.push(
+    new ethereum.EventParam(
+      "workScopes",
+      ethereum.Value.fromUnsignedBigIntArray(workScopes)
+    )
+  )
+  impactClaimedEvent.parameters.push(
+    new ethereum.EventParam(
+      "impactScopes",
+      ethereum.Value.fromUnsignedBigIntArray(impactScopes)
+    )
+  )
+  impactClaimedEvent.parameters.push(
+    new ethereum.EventParam(
+      "version",
+      ethereum.Value.fromUnsignedBigInt(version)
+    )
+  )
+  impactClaimedEvent.parameters.push(
+    new ethereum.EventParam("uri", ethereum.Value.fromString(uri))
+  )
+
+  return impactClaimedEvent
+}
+
+export function createInitializedEvent(version: i32): Initialized {
+  let initializedEvent = changetype<Initialized>(newMockEvent())
+
+  initializedEvent.parameters = new Array()
+
+  initializedEvent.parameters.push(
+    new ethereum.EventParam(
+      "version",
+      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(version))
+    )
+  )
+
+  return initializedEvent
+}
+
+export function createRoleAdminChangedEvent(
+  role: Bytes,
+  previousAdminRole: Bytes,
+  newAdminRole: Bytes
+): RoleAdminChanged {
+  let roleAdminChangedEvent = changetype<RoleAdminChanged>(newMockEvent())
+
+  roleAdminChangedEvent.parameters = new Array()
+
+  roleAdminChangedEvent.parameters.push(
+    new ethereum.EventParam("role", ethereum.Value.fromFixedBytes(role))
+  )
+  roleAdminChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousAdminRole",
+      ethereum.Value.fromFixedBytes(previousAdminRole)
+    )
+  )
+  roleAdminChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "newAdminRole",
+      ethereum.Value.fromFixedBytes(newAdminRole)
+    )
+  )
+
+  return roleAdminChangedEvent
+}
+
+export function createRoleGrantedEvent(
+  role: Bytes,
+  account: Address,
+  sender: Address
+): RoleGranted {
+  let roleGrantedEvent = changetype<RoleGranted>(newMockEvent())
+
+  roleGrantedEvent.parameters = new Array()
+
+  roleGrantedEvent.parameters.push(
+    new ethereum.EventParam("role", ethereum.Value.fromFixedBytes(role))
+  )
+  roleGrantedEvent.parameters.push(
+    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
+  )
+  roleGrantedEvent.parameters.push(
+    new ethereum.EventParam("sender", ethereum.Value.fromAddress(sender))
+  )
+
+  return roleGrantedEvent
+}
+
+export function createRoleRevokedEvent(
+  role: Bytes,
+  account: Address,
+  sender: Address
+): RoleRevoked {
+  let roleRevokedEvent = changetype<RoleRevoked>(newMockEvent())
+
+  roleRevokedEvent.parameters = new Array()
+
+  roleRevokedEvent.parameters.push(
+    new ethereum.EventParam("role", ethereum.Value.fromFixedBytes(role))
+  )
+  roleRevokedEvent.parameters.push(
+    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
+  )
+  roleRevokedEvent.parameters.push(
+    new ethereum.EventParam("sender", ethereum.Value.fromAddress(sender))
+  )
+
+  return roleRevokedEvent
+}
+
+export function createTransferBatchEvent(
+  operator: Address,
+  from: Address,
+  to: Address,
+  ids: Array<BigInt>,
+  values: Array<BigInt>
+): TransferBatch {
+  let transferBatchEvent = changetype<TransferBatch>(newMockEvent())
+
+  transferBatchEvent.parameters = new Array()
+
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam("ids", ethereum.Value.fromUnsignedBigIntArray(ids))
+  )
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam(
+      "values",
+      ethereum.Value.fromUnsignedBigIntArray(values)
+    )
+  )
+
+  return transferBatchEvent
+}
+
+export function createTransferSingleEvent(
+  operator: Address,
+  from: Address,
+  to: Address,
+  id: BigInt,
+  value: BigInt
+): TransferSingle {
+  let transferSingleEvent = changetype<TransferSingle>(newMockEvent())
+
+  transferSingleEvent.parameters = new Array()
+
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("id", ethereum.Value.fromUnsignedBigInt(id))
+  )
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return transferSingleEvent
+}
+
+export function createURIEvent(value: string, id: BigInt): URI {
+  let uriEvent = changetype<URI>(newMockEvent())
+
+  uriEvent.parameters = new Array()
+
+  uriEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromString(value))
+  )
+  uriEvent.parameters.push(
+    new ethereum.EventParam("id", ethereum.Value.fromUnsignedBigInt(id))
+  )
+
+  return uriEvent
+}
+
+export function createUpgradedEvent(implementation: Address): Upgraded {
+  let upgradedEvent = changetype<Upgraded>(newMockEvent())
+
+  upgradedEvent.parameters = new Array()
+
+  upgradedEvent.parameters.push(
+    new ethereum.EventParam(
+      "implementation",
+      ethereum.Value.fromAddress(implementation)
+    )
+  )
+
+  return upgradedEvent
+}
