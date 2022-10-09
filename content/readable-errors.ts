1673cbad7b18a832bd4585fdaa9a55f8bbf46d9d
jipstavenuiter <jipstavenuiter@gmail.com>
2022-10-09 18:57:19 +0200

    add error message for conflicting claims

--- content/readable-errors.ts
+++ content/readable-errors.ts
@@ -1,3 +1,4 @@
 export const errorMessages: Record<string, string | undefined> = {
   AlreadyMinted: "This HyperCert has already been claimed",
+  ConflictingClaim: "This claim conflicts with an existing claim",
 };
