cf354651e12a5449d7add6318a08031b6d572bca
jipstavenuiter <jipstavenuiter@gmail.com>
2022-10-14 09:17:34 +0200

    enable browse page when not connected

--- hooks/contracts.ts
+++ hooks/contracts.ts
@@ -1,11 +1,17 @@
 import { useTypedContract } from "@raidguild/quiver";
 import { HyperCertMinter__factory } from "../contract-types";
-import { CONTRACT_ADDRESS } from "../constants";
+import { CONTRACT_ADDRESS, DEFAULT_CHAIN_ID } from "../constants";
 
 export const useHypercertContract = () => {
   const { contract } = useTypedContract(
     CONTRACT_ADDRESS,
-    HyperCertMinter__factory
+    HyperCertMinter__factory,
+    {
+      staticProvider: {
+        enable: true,
+        chainId: DEFAULT_CHAIN_ID,
+      },
+    }
   );
 
   return contract;
