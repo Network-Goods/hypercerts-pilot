d7f4cb3b1c43188b9184a8526f82c5bf908b2a3a
jipstavenuiter <jipstavenuiter@gmail.com>
2023-01-09 16:07:14 +0100

    bump sdk version

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   env: {
     NFT_STORAGE_TOKEN: process.env.NEXT_PUBLIC_NFT_STORAGE_TOKEN,
   },
+  transpilePackages: ["@network-goods/hypercert-sdk"],
 };
 
 module.exports = nextConfig;
