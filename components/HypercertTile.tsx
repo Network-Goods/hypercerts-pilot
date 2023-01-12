f23f6f478bf6191160113212175b9ce47563be12
jipstavenuiter <jipstavenuiter@gmail.com>
2023-01-12 16:58:09 +0100

    disable links on my-tokens page

--- components/HypercertTile.tsx
+++ components/HypercertTile.tsx
@@ -6,15 +6,21 @@ export const HypercertTile = ({
   id,
   uri,
   hoverEffect,
+  disabled = false,
 }: {
   id: string;
   uri: string;
   hoverEffect?: boolean;
+  disabled?: boolean;
 }) => {
   const { data } = useClaimMetadata(uri);
 
   if (!data) {
     return null;
+  }
+
+  if (disabled) {
+    return <Tile src={data.image} hoverEffect={hoverEffect} />;
   }
 
   return (
